3b836d34210b916ef5b7f82885d2f9b89efbb84d
Artyzh <artyzh@inbox.ru>
2022-11-27 21:37:07 +0300

    Enable filling page of links of forms

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
 
-PATH = "/home/artemiyz/Programming/chromedriver"
+PATH = "./chromedriver"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLSflKyNIuB323TF_5OWhFOwuxu_y_Ig-EXZJaYxDixZ6ZlDcKA/viewform")
+driver.get("file:///home/artemiyz/Programming/School/FF2/MASH.html")
 print("Connected to ", driver.title)
 
 # driver.switch_to.new_window('tab')
 
-for i in range(20):
-    driver.refresh()
-    inp = driver.find_element(By.CSS_SELECTOR, "input[type='text']")
-    sub = driver.find_element(By.XPATH, '//span[contains(text(), "Отправить")]')
-    inp.send_keys('dsfvfgfs')
-    ActionChains(driver).click(sub).perform()
-    time.sleep(0.5)
+try:
 
-driver.quit()
+    hrefs = [i.get_attribute('href') for i in driver.find_elements(By.CSS_SELECTOR, '.form')]
+    home = driver.current_window_handle
+
+    cnt = 0
+    for i in hrefs:
+        driver.switch_to.new_window('tab')
+        driver.get(i)
+        inp = driver.find_element(By.CSS_SELECTOR, "input[type='text']")
+        sub = driver.find_element(By.XPATH, '//span[contains(text(), "Отправить")]')
+        inp.send_keys('dsfvfgfs')
+        ActionChains(driver).click(sub).perform()
+        time.sleep(0.5)
+        print('Completed %d forms' % (cnt := cnt + 1))
+        driver.close()
+        driver.switch_to.window(home)
+except:
+    print('Terminated')
+
+finally:
+    driver.quit()
